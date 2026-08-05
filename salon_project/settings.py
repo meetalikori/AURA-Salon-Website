4f239757807244533650b2dee1aee24eb5d930f6
meetalikori <meetalikori2004@gmail.com>
2026-08-05 23:04:05 +0530

    Update ALLOWED_HOSTS for deployment

--- salon_project/settings.py
+++ salon_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g^*k&9=gni#0k14(ev7r1*jd#_4^gz+uijxbxfkn#5t#3p^=)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
